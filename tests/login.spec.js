dc9ea26e7fc7b606be433475c82057992a2df141
Nigel <nigeldave19@gmail.com>
2025-12-23 23:40:38 +0530

    Create login.spec.js

---
+++ tests/login.spec.js
@@ -0,0 +1,58 @@
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import { browser } from 'vibium'
+const fs = require('fs')
+const { browserSync } = require('vibium')
+
+var vibe
+
+describe('Verify that login works correctly', () => { 
+
+    beforeAll(async () => {
+
+        // Launch a browser (you'll see it open!)
+        vibe = await browser.launch()
+                
+        // Go to a website
+        vibe.go('https://www.saucedemo.com/')
+        console.log('Loaded sauce demo')
+
+        // Take a screenshot
+        const png1 = vibe.screenshot()
+        fs.writeFileSync('screenshot1.png', png1)
+        console.log('Saved screenshot1.png')
+    })
+
+
+    afterAll(async () => {
+
+        // Close the browser
+        await vibe.quit()
+        console.log('Done!')
+    })
+
+    it('should login successfully with valid credentials', async () => {
+    
+        // Login
+        vibe.find('#user-name').type('standard_user')
+        vibe.find('#password').type('secret_sauce')
+        vibe.find('#login-button').click()
+    
+        const app_logo = vibe.find('.app_logo')
+    
+        console.log('Found App Logo: ', app_logo.text())
+        console.log('Logged in!')
+    
+        // Take a screenshot
+        const png2 = vibe.screenshot()
+        fs.writeFileSync('screenshot2.png', png2)
+        console.log('Saved screenshot2.png')
+    })
+
+
+
+
+
+
+
+
+});
